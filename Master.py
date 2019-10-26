a015a0f7b861a4a652e5d49218fc50bb75111990
SergioGhisler <39150585+SergioGhisler@users.noreply.github.com>
2019-10-26 22:40:52 +0200

    Soporte multiple de archivos y busqueda del archivo en borrar

--- Master.py
+++ Master.py
@@ -17,14 +17,15 @@ class Master:
     ############################################
 
     def read(self, *args):
-        nombre=self.database.find(args[0][0])
+        
         aux=self.database.split(";")
+        nombre=aux.index(args[0][0])
+        del aux[-1]
         j=0
         
-        ##ARRGELAR ESTO
-        aux.pop(0)
-        aux.pop()    
-        ####################
+        lon=int(aux[nombre+1])
+        aux=aux[nombre+2:nombre+2+lon]
+       
         resultado=""
         
         
@@ -40,21 +41,37 @@ class Master:
         return resultado
 
     def write(self, *args):
+
+        j=0
+        m=0
+        #Si ya hay algo guardado en memoria seguir por donde lo dejol
+        if(self.database!=''):
+            sep=self.database.split(";")
+            del sep[-1]
+            a= sep[-1].split(":")
+            j=int(a[0][1 : : ])
+             
+            m=int(a[1])+1
+            #Por si el ultimo nodo esta a tope
+            if(len(self.slaveDB["S"+str(j)].database)==self.slaveDB["S"+str(j)].memory):
+                j=j+1
+                m=0
+           
+        
         f = open(args[0][0])
         aux=f.read()
-        self.database += f.buffer.name+" ;"
+        z= math.ceil(len(aux)/self.memoryBlock)
+        # la z la anadimos para tener el metadato de la longitud
+        self.database += f.buffer.name+";"+str(z)+";"
 
-        j= math.ceil(len(aux)/self.memoryBlock)
+        
         dict={}
-        for k in range(j):
-            dict["S"+str(k)] = aux[0:self.memoryBlock]
+        for k in range(z):
+            dict["S"+str(k)] = aux[0:self.memoryBlock].ljust(self.memoryBlock)
 
             aux = aux[self.memoryBlock ::]
 
-        for i in dict:
-            print (dict[i])
-        j=0
-        m=0
+        
         for i in dict:
             
             self.slaveDB["S"+str(j)].write(dict[i])
