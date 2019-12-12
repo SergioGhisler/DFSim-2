cb699f1d2dae95871b05bdce15882851ddd466c8
SergioGhisler <39150585+SergioGhisler@users.noreply.github.com>
2019-12-12 12:17:09 +0100

    primero

--- Master.py
+++ Master.py
@@ -22,9 +22,85 @@ class Master:
     #       MODIFICAR A PARTIR DE AQUI         #
     ############################################
 
+
+    def mapReduce(self,*args):
+        operacion = args[0][1]
+
+        aux = self.database.split(";")
+        factor_replicacion=int(aux[0])
+        if not args[0][0]+"$%&" in aux:
+            print("No existe ese archivo")
+            return None
+        nombre = aux.index(args[0][0]+"$%&")
+        del aux[-1]
+        
+
+        lon = int(aux[nombre+1])
+        aux = aux[nombre+2:nombre+2+lon]
+
+        
+        aux2={}
+        contador=0
+        while True:
+            aux2[contador]=(aux[0:factor_replicacion])
+            del aux[0:factor_replicacion]
+            contador+=1
+            if len(aux)==0:
+                break
+        d={}
+        for i in aux2:
+            mindict= aux2[i]
+            for j in range(0,len(aux2)):
+                a = mindict[j].split(":")
+                slave = a[0]
+                pos = int(a[1])
+                hasheo = int(a[2])
+                if(hasheo==hash(self.slaveDB[slave].database[pos*int(self.memoryBlock):pos*int(self.memoryBlock)+int(self.memoryBlock)])):
+                    busqueda=j
+                
+                    break
+            
+            a = mindict[j].split(":")
+            slave = a[0]
+            pos = a[1]
+            hasheo = a[2]
+
+            
+
+            
+            #Lo que es el mapReduce empieza aqui, antes solo hemos accedido al igual que en el leer al texto.
+            map= self.slaveDB[slave].map(pos, self.memoryBlock,operacion)
+            for i in map:
+                if i in d:
+                    d[i].append(tuple((i,map[i])))
+                else:
+                    d[i]=[(i,map[i])]
+            contador=0
+        resultado={}
+        
+        for i in d:
+            resultado.update(self.slaveDB['S'+str(contador)].reduce(d[i],operacion))
+            if contador<len(self.slaveDB)-1:
+                contador+=1
+            else:
+                contador=0
+        resultado=list(resultado.items())
+        if operacion=='palabras':
+            
+            aux=list(sorted( ((v,k) for k,v in resultado)))
+            resultado=[]
+            for i in aux:
+
+                resultado.append(i[1])
+        print(resultado)
+
+        return resultado
+
     def read(self, *args):
+        
         tiempoInicial= time()
         aux = self.database.split(";")
+        factor_replicacion=int(aux[0])
         if not args[0][0]+"$%&" in aux:
             print("No existe ese archivo")
             return None
@@ -36,12 +112,33 @@ class Master:
         aux = aux[nombre+2:nombre+2+lon]
 
         resultado = ""
+        aux2={}
+        contador=0
 
-        for i in aux:
-
-            a = i.split(":")
+        ##Para hacer un diccionario de tuplas del mismo fragmento de texto
+        while True:
+            aux2[contador]=(aux[0:factor_replicacion])
+            del aux[0:factor_replicacion]
+            contador+=1
+            if len(aux)==0:
+                break
+            
+        for i in aux2:
+            mindict= aux2[i]
+            for j in range(0,len(aux2)):
+                a = mindict[j].split(":")
+                slave = a[0]
+                pos = int(a[1])
+                hasheo = int(a[2])
+                if(hasheo==hash(self.slaveDB[slave].database[pos*int(self.memoryBlock):pos*int(self.memoryBlock)+int(self.memoryBlock)])):
+                    busqueda=j
+                
+                    break
+            
+            a = mindict[j].split(":")
             slave = a[0]
             pos = a[1]
+            hasheo = a[2]
 
             resultado = resultado + \
                 self.slaveDB[slave].read(pos, self.memoryBlock)
@@ -56,38 +153,15 @@ class Master:
         tiempoTotal= tiempoFinal-tiempoInicial
         print("lectura"+str(tiempoTotal))
         return None
-    def wirteSecuencial(self, *args):
-        tiempoInicial= time()
-        j = 0
-        for k in args[0]:
-            successful = False
-            while not successful:
-                if len(self.slaveDB['S'+str(j)].database) == self.slaveDB['S'+str(j)].memory:
-                    if(j==len(self.slaveDB)-1):
-                        j=0
-                    else:
-                        j += 1
-                    
-                else:
-                    texto = args[0][k]
-                    pos = int(self.slaveDB['S'+str(j)].write(texto, self.memoryBlock))
-                    self.database += 'S'+str(j)+':'+str(pos)+";"
-                    
-                    if(j==len(self.slaveDB)-1):
-                        j=0
-                    else:
-                        j += 1
-                    successful=True
-                
-        tiempoFinal=time()
-        tiempoTotal= tiempoFinal-tiempoInicial
-        print("escritura"+str(tiempoTotal))
 
         
     def writeAleatorio(self,*args):
+        factor_replicacion= int(args[1])
         tiempoInicial= time()
+        
         j = 0
         noLlenos=dict(self.slaveDB)
+        
         for k in args[0]:
             aux=random.choice(list(noLlenos.keys()))
             successful = False
@@ -97,52 +171,20 @@ class Master:
                     aux=random.choice(list(noLlenos.keys()))
                     
                 else:
-                    texto = args[0][k]
-                    pos = int(self.slaveDB[aux].write(texto, self.memoryBlock))
-                    self.database += aux+':'+str(pos)+";"
+                    noUsado=dict(self.slaveDB)
+
+                    for i in range(0,factor_replicacion):
+                        texto = args[0][k]
+                        pos = int(self.slaveDB[aux].write(texto, self.memoryBlock))
+                        self.database += aux+':'+str(pos)+":"+str(hash(texto))+";"
+                        del noUsado[aux]
+                        aux=random.choice(list(noUsado.keys()))
+                        
                     successful=True
         tiempoFinal=time()
         tiempoTotal= tiempoFinal-tiempoInicial
-        print("escritura"+str(tiempoTotal))           
-    def writePrimeroVacio(self, *args):
-        tiempoInicial= time()
-        vuelta=0
-        for i in args[0]:
-            texto=args[0][i]
-
-            for j in self.slaveDB:
-                pos=int(len(self.slaveDB[j].database)/self.memoryBlock)
-                if(pos!=vuelta):
-                    if j== list(self.slaveDB)[len(self.slaveDB)-1]:
-                        vuelta+=1
-                    continue
-
-                else:
-                    self.slaveDB[j].write(texto,self.memoryBlock)
-                    self.database+=j+':'+str(pos)+";"
-                    if j== list(self.slaveDB)[len(self.slaveDB)-1]:
-                        vuelta+=1
-                    break
-
-        tiempoFinal=time()
-        tiempoTotal= tiempoFinal-tiempoInicial
-        print("escritura"+str(tiempoTotal))     
-    def writeMaximaCarga(self,*args):
-        tiempoInicial= time()
-        for i in args[0]:
-            texto=args[0][i]
-            for j in self.slaveDB:
-                if len(self.slaveDB[j].database)==self.slaveDB[j].memory:
-                    continue
-                else:
-                    pos=int(self.slaveDB[j].write(texto,self.memoryBlock))
-                    self.database+=j+':'+str(pos)+";"
-                    break
-
-        tiempoFinal=time()
-        tiempoTotal= tiempoFinal-tiempoInicial
-        print("escritura"+str(tiempoTotal))     
-
+        print("escritura"+str(tiempoTotal))         
+   
 
 
 
@@ -150,15 +192,17 @@ class Master:
         
 
     def write(self, *args):
+        if self.database=='':
+            factor_de_replicacion= input("Introduzca el factor de replicacion del sisrtema: ")
+            self.database=factor_de_replicacion+';'
+        #Total de la memoria total del sistema
         memoriaMaxima = int(len(self.slaveDB)*(self.slaveDB[list(self.slaveDB)[0]].memory/self.memoryBlock))
-        
-        
-        
+
         switcher = {
-            1: self.writePrimeroVacio,
+           ### 1: self.writePrimeroVacio,
             2: self.writeAleatorio,
-            3: self.wirteSecuencial,
-            4: self.writeMaximaCarga
+            ###3: self.wirteSecuencial,
+            ###4: self.writeMaximaCarga
         }
         if not os.path.isfile(args[0][0]):
             print('Ese archivo no existe')
@@ -166,7 +210,9 @@ class Master:
         f = open(args[0][0])
         
         # la z la anadimos para tener el metadato de la longitud
+        # Z es el número de "huecos" que utiliza el texto
         aux=f.read()
+        
         z= math.ceil(len(aux)/self.memoryBlock)
         aux2= self.database.split(";")
         if  args[0][0]+"$%&" in aux2:
@@ -179,18 +225,21 @@ class Master:
 
             aux = aux[self.memoryBlock ::]
 
+        factor_replicacion = aux2[0]
         
         i=0
         for j in aux2:
             if '$%&' in j:
                 i+=1
         i=i*2
-        huecosRestantes=memoriaMaxima-(len(aux2)-i)
-        if z<huecosRestantes:
-            self.database += f.buffer.name+"$%&;"+str(z)+";"
-            command = input("Tipo--> ") 
+        #+1 pq siempre hay un hueco vacio en el diccionario
+        huecosRestantes=memoriaMaxima-(len(aux2)-i)+1
+        if z*int(factor_replicacion)<huecosRestantes:
+            self.database += f.buffer.name+"$%&;"+str(z*int(factor_replicacion))+";"
+            command = 2
+            '''input("Tipo--> (1: Primero vacio 2: Aleatorio 3:Secuencial 4: Máxima carga) ") '''
         
-            output = switcher[int(command)](dict)
+            output = switcher[int(command)](dict,factor_replicacion)
         else:
             print("No queda espacio para este texto")
             output = "none"
@@ -199,46 +248,3 @@ class Master:
         
 
 
-    def delete(self, *args):
-            
-        aux=self.database.split(";")
-        nombre=aux.index(args[0][0]+"$%&")
-        del aux[-1]
-        j=0
-        
-        lon=int(aux[nombre+1])
-        aux2=aux[nombre+2:nombre+2+lon]
-       
-        get_indexes = lambda x, xs: [i for (y, i) in zip(xs, range(len(xs))) if x in y]
-        
-        
-        for i in reversed(aux2):
-            
-            a=i.split(":")
-            slave=a[0]
-            pos=int(a[1])
-            max=int(self.slaveDB[slave].memory/self.memoryBlock)-1
-            if pos!=max:
-                if(len(self.slaveDB[slave].database)==self.database):
-                    self.slaveDB[slave].delete(pos,self.memoryBlock)
-                else:
-                    indices=get_indexes(slave+':',aux)
-                    
-                    for p in indices:
-                        if int(aux[p].split(':')[1])>pos:
-                            
-                            aux[p]=aux[p].split(':')[0]+':'+str(int(aux[p].split(':')[1])-1)
-                            
-                        
-                        
-                        
-
-            self.slaveDB[slave].delete(pos,self.memoryBlock)
-
-        del aux[nombre:nombre+2+lon]
-        
-        self.database=';'.join(aux)
-        if aux:
-            self.database+=';'
-        print(self.database)
-        return None
